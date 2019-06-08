971e102bd2c37a1bbf8cdc7714e96299a26d2f03
t-rekttt <t.to.the.rekt@gmail.com>
2019-06-08 16:08:59 +0700

    Refactor and added apk for installation

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 let network = require('network');
 let shell = require('shelljs');
 
-network.get_active_interface(function(err, interface) {
-  if (err) return console.log(err);
+let getActiveInterface = () => {
+  return new Promise(resolve => {
+    network.get_active_interface((err, interface) => {
+      if (err) throw err;
 
-  let { ip_address } = interface;
+      resolve(interface);
+    });
+  });
+}
+(async() => {
+  let { ip_address } = await getActiveInterface();
 
-  shell.exec(`adb shell am start -n tk.elevenk.proxysetter/.MainActivity -e host ${ip_address} -e port 8080`);
-});
+  let packageExec = shell.exec('adb shell pm list packages -f | grep tk.elevenk.proxysetter', { silent: true });
+
+  if (packageExec.code !== 0) {
+    throw packageExec.stderr;
+  }
+
+  if (!packageExec.stdout || packageExec.stdout.split('\n') == 0) {
+    console.log('Proxy apk not installed, installing...');
+
+    shell.exec(`adb install ${__dirname}/proxy-setter-debug-0.2.1.apk`);
+  }
+
+  console.log(`Setting proxy ${ip_address}:8080`);
+
+  let result = shell.exec(`adb shell am start -n tk.elevenk.proxysetter/.MainActivity -e host ${ip_address} -e port 8080`, { silent: true });
+
+  if (result.code !== 0) {
+    throw stdout.err;
+  }
+
+  console.log('Setting proxy successfully!');
+})();
