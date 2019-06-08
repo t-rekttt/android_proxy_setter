be046592d6ef7066571e26ae18034de01ebc53f6
t-rekttt <t.to.the.rekt@gmail.com>
2019-06-08 17:11:18 +0700

    Fixing a bug when trying to check apk installed

--- index.js
+++ index.js
@@ -11,27 +11,27 @@ let getActiveInterface = () => {
   });
 }
 (async() => {
-  let { ip_address } = await getActiveInterface();
+  try {
+    let { ip_address } = await getActiveInterface();
 
-  let packageExec = shell.exec('adb shell pm list packages -f | grep tk.elevenk.proxysetter', { silent: true });
+    let packageExec = shell.exec('adb shell pm list packages -f | grep tk.elevenk.proxysetter', { silent: false });
 
-  if (packageExec.code !== 0) {
-    throw packageExec.stderr;
+    if (packageExec.code !== 0) {
+      console.log('Proxy apk not installed, installing...');
+
+      shell.exec(`adb install ${__dirname}/proxy-setter-debug-0.2.1.apk`);
+    }
+
+    console.log(`Setting proxy ${ip_address}:8080`);
+
+    let result = shell.exec(`adb shell am start -n tk.elevenk.proxysetter/.MainActivity -e host ${ip_address} -e port 8080`, { silent: true });
+
+    if (result.code !== 0) {
+      throw stdout.err;
+    }
+
+    console.log('Setting proxy successfully!');
+  } catch (err) {
+    console.log(err);
   }
-
-  if (!packageExec.stdout || packageExec.stdout.split('\n') == 0) {
-    console.log('Proxy apk not installed, installing...');
-
-    shell.exec(`adb install ${__dirname}/proxy-setter-debug-0.2.1.apk`);
-  }
-
-  console.log(`Setting proxy ${ip_address}:8080`);
-
-  let result = shell.exec(`adb shell am start -n tk.elevenk.proxysetter/.MainActivity -e host ${ip_address} -e port 8080`, { silent: true });
-
-  if (result.code !== 0) {
-    throw stdout.err;
-  }
-
-  console.log('Setting proxy successfully!');
 })();
